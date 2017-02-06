bb6316c5fc2ac7ae050d1b1a8469f559b88111ad
“lukemccrae” <lukemccrae@gmail.com>
2017-02-06 16:30:55 -0700

    found some guys code and got it working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,28 +18,52 @@ angular.module('starter.controllers', ['ngCordova'])
   };
 })
 
-.controller('ChatsCtrl', function($scope, $cordovaCamera) {
-  console.log("new chats ctrl");
+.controller('ChatsCtrl', function($scope, $cordovaCapture) {
+  // const vm = this;
+  // console.log($cordovaCapture);
+  //
+  // $scope.captureVideo = function() {
+  //   var options = { limit: 3, duration: 10 };
+  //
+  //   $cordovaCapture.captureVideo(options).then(function(videoData) {
+
+  //   }, function(err) {
+  //     // An error occurred. Show a message to the user
+  //   });
+  // }
+
+  document.addEventListener("deviceready", init, false);
+function init() {
 
 
+	document.querySelector("#takeVideo").addEventListener("touchend", function() {
+		console.log("Take video");
+		navigator.device.capture.captureVideo(gifshot.createGIF, captureError, {limit: 1});
+	}, false);
 
-  $scope.takePicture = function() {
-    var options = {
-        quality : 75,
-        destinationType : Camera.DestinationType.DATA_URL,
-        sourceType : Camera.PictureSourceType.CAMERA,
-        allowEdit : true,
-        encodingType: Camera.EncodingType.MP4,
-        targetWidth: 100,
-        targetHeight: 100,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: false
-    };
-
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-        $scope.imgURI = "data:image/jpeg;base64," + imageData;
-    }, function(err) {
-        // An error occured. Show a message to the user
-    });
 }
+
+function captureError(e) {
+	console.log("capture error: "+JSON.stringify(e));
+}
+
+// gifshot.createGIF(s, {
+//     video: [
+//       s[0].fullPath
+//     ]
+// }, function (obj) {
+//     if (!obj.error) {
+//         var image = obj.image, animatedImage = document.createElement('img');
+//         animatedImage.src = image;
+//         document.body.appendChild(animatedImage);
+//     }
+// });
+
+function captureSuccess(s) {
+	var v = "<video controls='controls'>";
+	v += "<source src='" +  + "' type='video/mp4'>";
+	v += "</video>";
+	document.querySelector("#videoArea").innerHTML = v;
+}
+
 })
